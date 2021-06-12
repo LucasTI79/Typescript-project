d07b4836c208f571eba5d90d47bd9ecf76502794
Lucas <lukasalves271@gmail.com>
2021-06-12 15:44:25 -0300

    add cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express'
+import cors from 'cors'
 import routes from './routes';
 
 const app = express();
 
+app.use(cors())
 app.use(routes);
 
 app.listen(3333, () => console.log(`app listening in http://localhost:3333`))
